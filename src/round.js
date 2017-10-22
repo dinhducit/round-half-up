17a123319393d241c8a6e129cd17d93aea658416
Matthew Morgan <matt@dulcetsoftware.com>
2017-10-22 19:05:09 -0400

    Refactor HALF_UP method

--- src/round.js
+++ src/round.js
@@ -7,18 +7,14 @@ const DIRECTIONS = {
 
 function round({number=0, direction=DIRECTIONS.HALF_UP, precision=2}) {
   const sign = Math.sign(number);
-  const numberString = (Math.abs(number)*(10**precision)).toFixed(1);
+  const power = 10**precision;
   switch (direction){
     default:
     case DIRECTIONS.HALF_UP: {
-      const roundingDigit = Number(numberString.slice(-1));
-      if (roundingDigit < 5){
-        return Math.floor(+numberString) / 10**precision * sign;
-      } else {
-        return Math.floor(+numberString + 1) / 10**precision * sign;
-      }
+      return sign * Math.round(Math.abs(number) * power) / power;
     }
   }
 }
+
 
 export { round, DIRECTIONS };
